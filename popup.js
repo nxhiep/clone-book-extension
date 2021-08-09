22027a13ac218d85322b42a64a6bae4ca832da15
Hiep Nguyen <nxhiep.1996@gmail.com>
2021-08-09 20:34:14 +0700

    hack face ok

--- popup.js
+++ popup.js
@@ -6,9 +6,6 @@ function constructOptions(kButtonColors) {
             let button = document.createElement('button');
             button.style.backgroundColor = item;
             button.addEventListener('click', function () {
-                // chrome.storage.sync.set({ color: item }, function () {
-                // });
-                console.log(222);
                 chrome.tabs.executeScript(
                     tabs[0].id,
                     { code: 'document.body.style.backgroundColor = "'+item+'";' });
@@ -22,7 +19,6 @@ constructOptions(kButtonColors);
 let test = document.getElementById('test');
 test.onclick = function (element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        console.log("test");
         chrome.tabs.executeScript(
             tabs[0].id,
             { code: 'document.body.style.backgroundColor = "#e8453c";' });
