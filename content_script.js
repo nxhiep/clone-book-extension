fd764bc3cdde72b6915cb687398e80175b73e0a1
Hiep Nguyen <nxhiep.1996@gmail.com>
2021-09-18 11:33:18 +0700

    add amazon, uniontestprep

--- content_script.js
+++ content_script.js
@@ -1,126 +1,148 @@
-if(window.location.href.indexOf('amazon.com') > -1){
-    var div = document.createElement('div')
-    div.style.position = 'fixed'
-    div.style.top = '0'
-    div.style.left = '0'
-    div.style.width = '100px'
-    div.style.height = '50px'
-    div.style.zIndex = "999999"
-    var inputOffset = document.createElement('input')
-    inputOffset.placeholder = 'Offset'
-    inputOffset.value = 1
-    div.appendChild(inputOffset)
-    var inputLimit = document.createElement('input')
-    inputLimit.placeholder = 'Limit'
-    inputLimit.value = 10
-    div.appendChild(inputLimit)
-    var button = document.createElement('button')
-    button.innerHTML = "Start copy"
-    document.body.appendChild(div)
-    div.appendChild(button)
-    button.onclick = startCopy
-    // button.onclick = () => {
-    //     var iframe = document.getElementById('KindleReaderIFrame')
-    //     var _document = iframe.contentWindow.document
-    //     let content = getContentFromIframe(_document.getElementById('kindleReader_content'));
-    //     console.log("content ", content);
-    // }
-    function startCopy () {
-        let content = ''
-        let page = 1
-        try {
-            page = parseInt(inputOffset.value)
-        } catch(e){}
-        if(page <= 0){
-            page = 1
-        }
-        let timer = setInterval(async () => {
-            let maxPage = 10;
-            try {
-                maxPage = parseInt(inputLimit.value)
-            } catch(e){}
-            if(maxPage <= 0){
-                alert("IQ 200!")
-                return;
-            }
-            var iframe = document.getElementById('KindleReaderIFrame')
-            var _document = iframe.contentWindow.document
-            let dialog = _document.getElementById('kindleReader_dialog_goto_page')
-            let b1 = _document.getElementById('kindleReader_button_goto');
-            b1 && b1.click()
-            let b2 = _document.getElementById('kindleReader_goToMenuItem_goToPage');
-            b2 && b2.click()
-            dialog = _document.getElementById('kindleReader_dialog_goto_page').parentElement
-            await sleep(200);
-            let input = _document.getElementById('kindleReader_dialog_gotoPageField')
-            input.value = page
-            dialog.querySelectorAll('.ui-dialog-buttonset button')[1].click()
-            console.log("load page ", page)
-            await sleep(500);
-            content += getContentFromIframe(_document.getElementById('kindleReader_content'))
-            page += 1;
+const KEY = 'clone_data_hello_world';
 
-            if(page >= maxPage){
-                var win = window.open("", "Title", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=200,top="+(screen.height-400)+",left="+(screen.width-840));
-                // win.document.body.innerHTML = '<ul>' + content + '</ul>'
-                content = '';
-                let keys = Object.keys(mapContent).sort((a, b) => mapIndex[a] - mapIndex[b])
-                for(let key of keys){
-                    content += mapContent[key]
-                }
-                win.document.body.innerHTML = '<ul style="list-style-type: none;">' + content + '</ul>'
-                clearInterval(timer)
-            }
-        }, 1500)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getTopicName() {
+    let name = document.querySelector('.tp-display-section-name');
+    return name && name.innerText || '';
+}
+
+function getQuestionId() {
+    let prefix = '/question/';
+    let hash = window.location.hash;
+    let offset = hash.indexOf(prefix) + prefix.length;
+    let limit = hash.indexOf('?zoom');
+    return hash.substring(offset, limit);
+}
+
+var stop = true;
+
+if (window.location.href.indexOf('practicetest') > -1) {
+    let buttonStart = document.createElement('button');
+    buttonStart.innerHTML = 'Start';
+    buttonStart.onclick = () => {
+        stop = false;
+        selectQuestion();
     }
-    var mapContent = {}
-    var mapIndex = {}
-    var mapImg = {}
-    function getContentFromIframe(document) {
-        let content = ''
-        let iframes = document.querySelectorAll('iframe')
-        for(let iframe of iframes){
-            let _document = iframe.contentWindow.document
-            if(_document){
-                console.log("=============================================");
-                let ol = _document.querySelector('body > ol');
-                if(ol) {
-                    let lis = _document.querySelectorAll('body > ol > li');
-                    if(!lis || lis.length <= 1) {
-                        break;
-                    }
-                    for(let li of lis) {
-                        let id = li.getAttribute('value');
-                        id = id ? parseInt(id) : 0
-                        let question = "";
-                        for(let x of li.querySelectorAll(':scope > .k4w')) {
-                            question += x.innerText + " ";
-                        }
-                        question = question.trim();
-                        let imgs = li.querySelectorAll("img");
-                        let images = "";
-                        for(let img of imgs) {
-                            images += img.outerHTML;
-                        }
-                        if(question && !mapContent[question]) {
-                            let c = "<div class='question'><div class='title'>" + id + ". " + question + "</div>";
-                            c += "<div class='images'>" + images + "</div>";
-                            for(let x of li.querySelectorAll("ol>li")) {
-                                c += "<div class='choices' style='padding-left:20px'>" + x.innerText + "</div>";
-                            }
-                            c += "</div>";
-                            content += c;
-                            mapContent[question] = c;
-                            mapIndex[question] = id;
-                            mapImg[question] = images;
-                        }
-                    }
-                }
+    buttonStart.setAttribute('style', `
+        position: fixed;
+        top: 10px;
+        left: 10px;
+        padding: 10px 30px;
+        border-radius: 10px;
+        background: green;
+        color: white;
+        z-index: 1111
+    `);
+    let buttonStop = document.createElement('button');
+    buttonStop.innerHTML = 'Stop';
+    buttonStop.onclick = () => {
+        stop = true;
+    }
+    buttonStop.setAttribute('style', `
+        position: fixed;
+        top: 10px;
+        left: 120px;
+        padding: 10px 30px;
+        border-radius: 10px;
+        background: red;
+        color: white;
+        z-index: 1111
+    `);
+    document.body.appendChild(buttonStart);
+    document.body.appendChild(buttonStop);
+}
+
+async function selectQuestion() {
+    let formElement = document.querySelector('.question-container');
+    if (!formElement) {
+        window.alert('OK');
+        return;
+    }
+    await sleep(500);
+    let choices = formElement.querySelectorAll('.tp-question-multiplechoice .choice-container');
+    console.log(choices.length);
+    let check = Object.values(choices).find((a) => a.classList.contains('correct-answer'));
+    if (!check && choices.length > 0) {
+        let input = choices.item(0).querySelector('input');
+        input && input.click();
+        let buttonNext = document.querySelector('.tp-right-aligned-buttons #submitBtn');
+        buttonNext && buttonNext.click();
+    }
+    let question = await getQuestion(formElement);
+    console.log("question", question)
+    let buttonTryQuestion = document.querySelector('.tp-right-aligned-buttons .tp-nav-retry');
+    if(buttonTryQuestion) {
+        buttonTryQuestion.click();
+        await sleep(300);
+        let index = 1;
+        while(index < choices.length) {
+            await sleep(300);
+            let input = choices.item(index)?.querySelector('input');
+            input && input.click();
+            await sleep(300);
+            let buttonNext = document.querySelector('.tp-right-aligned-buttons #submitBtn');
+            buttonNext && buttonNext.click();
+            await sleep(300);
+            buttonTryQuestion = document.querySelector('.tp-right-aligned-buttons .tp-nav-retry');
+            console.log("xxxxxxx", index, !!buttonTryQuestion)
+            if(buttonTryQuestion) {
+                index++;
+                buttonTryQuestion.click();
+            } else {
+                break;
             }
         }
-        return content
     }
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+    let buttonNextQuestion = document.querySelector('.tp-right-aligned-buttons .tp-nav-next');
+    console.log("buttonNextQuestion", buttonNextQuestion)
+    fetch("http://localhost:4000/save-data", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(question)
+    }).then(res => {
+        console.log("Request complete! response:", res);
+        if (res.status == 200) {
+            setTimeout(() => {
+                buttonNextQuestion && buttonNextQuestion.click();
+            }, 300);
+            setTimeout(() => {
+                if(!stop) {
+                    selectQuestion();
+                }
+            }, 600);
+        } else {
+            window.alert('xxxxxxxxxxxxxxxxxxxxxxx')
+        }
+    });
+    return null;
+}
+
+async function getQuestion(formElement) {
+    await sleep(400);
+    let text = formElement.querySelector('.instruction').innerHTML;
+    let imageElement = formElement.querySelector('img');
+    console.log("imageElement", imageElement)
+    let image = imageElement && imageElement.getAttribute('src') || '';
+    let correctAnswer = '';
+    let answers = Object.values(formElement.querySelectorAll('.choice-container')).map(e => {
+        let text = e.querySelector('span[bind-compiled-html="opt.text"]');
+        text = text && text.innerHTML || '';
+        if(e.classList.contains('correct-answer')) {
+            correctAnswer = text;
+        }
+        return text;
+    });
+    return {
+        id: getQuestionId(),
+        topicName: getTopicName(),
+        text: text,
+        answer: answers,
+        correctAnswer: correctAnswer,
+        image: image,
     }
 }
