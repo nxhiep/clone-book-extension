d32debf961b120ee6fb048d0033a0dc5b6a4b00f
hiep.nx <nxhiep.1996@gmail.com>
2021-11-20 18:44:52 +0700

    Update script-remote.js

--- script-remote.js
+++ script-remote.js
@@ -1,3 +1,16 @@
 window.onload = function() {
-  console.log('hello world');
+  function test() {
+      fetch(`https://shopee.vn/api/v4/order/get_all_order_and_checkout_list?limit=100&offset=50&xxx=true`, { method: 'get' })
+      .then(function(response) {
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          return response.json();
+      }).then((data) => {
+          console.log(111, data);
+      });
+  }
+  try {
+      test();
+  } catch(e){}
 }
