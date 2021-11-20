8c322cbab0432686fba9a374cd9aeddd60b29add
Hiep Nguyen <nxhiep.1996@gmail.com>
2021-11-20 18:58:48 +0700

    zozo remote version

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ time: new Date().getTime() }, function () {
-        console.log("The color is green.");
     });
 });
