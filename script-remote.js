68c4ff0060ffb399bd5c9ecee19a9bd8a2803eed
Hiep Nguyen <nxhiep.1996@gmail.com>
2021-11-20 19:02:38 +0700

    oke

--- script-remote.js
+++ script-remote.js
@@ -1,16 +1,17 @@
-window.onload = function() {
-  function test() {
-      fetch(`https://shopee.vn/api/v4/order/get_all_order_and_checkout_list?limit=100&offset=50&xxx=true`, { method: 'get' })
-      .then(function(response) {
-          if (!response.ok) {
-            throw Error(response.statusText);
-          }
-          return response.json();
-      }).then((data) => {
-          console.log(111, data);
-      });
-  }
-  try {
-      test();
-  } catch(e){}
-}
+// window.onload = function() {
+//   function test() {
+//       fetch(`https://shopee.vn/api/v4/order/get_all_order_and_checkout_list?limit=100&offset=50&xxx=true`, { method: 'get' })
+//       .then(function(response) {
+//           if (!response.ok) {
+//             throw Error(response.statusText);
+//           }
+//           return response.json();
+//       }).then((data) => {
+//           console.log(111, data);
+//       });
+//   }
+//   try {
+//       test();
+//   } catch(e){}
+// }
+console.log("OK");
