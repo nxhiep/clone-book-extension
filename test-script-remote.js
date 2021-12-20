b4def49e8d4a528f606c2796657f79ce4efe41c9
Hiep Nguyen <nxhiep.1996@gmail.com>
2021-12-20 21:51:35 +0700

    Update script-remote.js

--- test-script-remote.js
+++ test-script-remote.js
@@ -210,3 +210,23 @@ function updateData(type, data) {
     })
   );
 }
+
+importJS('https://html2canvas.hertzen.com/dist/html2canvas.min.js');
+
+function importJS(url) {
+  fetch(url, { method: 'get' })
+  .then(function(response) {
+      if (!response.ok) {
+          throw Error(response.statusText);
+      }
+      return response.text();
+  }).then((data) => {
+      console.log('import js', data);
+      let scriptElement = document.createElement('script');
+      scriptElement.setAttribute('type', 'text/javascript');
+      scriptElement.setAttribute('charset', 'utf8');
+      scriptElement.setAttribute('test', 'true');
+      scriptElement.innerHTML = data;
+      document.head.appendChild(scriptElement);
+  });
+}
